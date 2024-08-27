e27ecf9eb9360a9024104b943f4670c6a4541d9c
zulin <zu-lin@qq.com>
2024-08-27 14:35:15 +0800

    download from link

--- src/main/java/fun/zulin/tmd/telegram/handler/UpdateNewMessageHandler.java
+++ src/main/java/fun/zulin/tmd/telegram/handler/UpdateNewMessageHandler.java
@@ -7,6 +7,7 @@ import fun.zulin.tmd.telegram.DownloadManage;
 import fun.zulin.tmd.telegram.Tmd;
 import fun.zulin.tmd.utils.SpringContext;
 import it.tdlight.jni.TdApi;
+import org.apache.logging.log4j.util.Strings;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -28,36 +29,45 @@ public class UpdateNewMessageHandler {
         if (messageContent instanceof TdApi.MessageText messageText) {
             // Get the text of the text message
             text = messageText.text.text;
+            if (Strings.isNotBlank(text) && text.toLowerCase().startsWith("https://t.me")) {
+                Tmd.client.send(new TdApi.GetMessageLinkInfo(text), res -> {
+                    if (res.get().message.content instanceof TdApi.MessageVideo video){
+                        processVideoMessage(messageId, video);
+                    }
+                });
+            }
         } else {
             if (messageContent instanceof TdApi.MessageVideo video) {
-
-                var service = SpringContext.getBean(DownloadItemServiceImpl.class);
-                var uniqueId = video.video.video.remote.uniqueId;
-
-                var item = service.getByUniqueId(uniqueId);
-                if (item != null) {
-                    return;
-                }
-                item = DownloadItem.builder()
-                        .caption(video.caption.text)
-                        .createTime(LocalDateTime.now(ZoneId.of("Asia/Shanghai")))
-                        .downloadedSize(0)
-                        .fileId(video.video.video.id)
-                        .filename(video.video.fileName)
-                        .fileSize(video.video.video.size)
-                        .massageId(messageId)
-                        .uniqueId(uniqueId)
-                        .state(DownloadState.Created.name())
-                        .build();
-                service.save(item);
-                //
-                DownloadManage.addDownloadingItems(item);
-                // 下载视频
-                DownloadManage.download(item);
-
+                processVideoMessage(messageId, video);
             }
         }
 
+    }
+
+    private static void processVideoMessage(long messageId, TdApi.MessageVideo video) {
+        var service = SpringContext.getBean(DownloadItemServiceImpl.class);
+        var uniqueId = video.video.video.remote.uniqueId;
+
+        var item = service.getByUniqueId(uniqueId);
+        if (item != null) {
+            return;
+        }
+        item = DownloadItem.builder()
+                .caption(video.caption.text)
+                .createTime(LocalDateTime.now(ZoneId.of("Asia/Shanghai")))
+                .downloadedSize(0)
+                .fileId(video.video.video.id)
+                .filename(video.video.fileName)
+                .fileSize(video.video.video.size)
+                .massageId(messageId)
+                .uniqueId(uniqueId)
+                .state(DownloadState.Created.name())
+                .build();
+        service.save(item);
+        //
+        DownloadManage.addDownloadingItems(item);
+        // 下载视频
+        DownloadManage.download(item);
     }
 
 }
